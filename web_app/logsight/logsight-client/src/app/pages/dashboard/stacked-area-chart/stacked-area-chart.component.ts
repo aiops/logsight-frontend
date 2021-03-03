158a039b1396ff0c8fdefd6f450911fa17cf291b
sasho <sashonedelkoski@gmail.com>
2021-03-03 18:11:37 +0100

    design changes and added better input files openstack logs

--- web_app/logsight/logsight-client/src/app/pages/dashboard/stacked-area-chart/stacked-area-chart.component.ts
+++ web_app/logsight/logsight-client/src/app/pages/dashboard/stacked-area-chart/stacked-area-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { data } from './data';
-
+import * as shape from 'd3-shape'
 @Component({
   selector: 'stacked-area-chart',
   styleUrls: ['./stacked-area-chart.component.scss'],
@@ -15,12 +15,13 @@ export class StackedAreaChartComponent {
   yAxis: boolean = true;
   showYAxisLabel: boolean = false;
   showXAxisLabel: boolean = false;
+  showGridLines: boolean = false;
   xAxisLabel: string = 'Year';
   yAxisLabel: string = 'Population';
   timeline: boolean = true;
-
+  linearCurve = shape.curveBasis;
   colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+    domain: ['#fff000', '#e85d04', '#393bff', '#00e1ff']
   };
 
   constructor() {

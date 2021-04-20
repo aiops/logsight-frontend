5bd823b5268f68c583ff0b0a4a672909d03c56ba
snedelkoski <sashonedelkoski@gmail.com>
2021-04-20 13:22:42 +0200

    webapp frontend improvements

--- web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.page.ts
+++ web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {IntegrationService} from "../../@core/service/integration.service";
+import {AuthenticationService} from "../../auth/authentication.service";
+import {NotificationsService} from "angular2-notifications";
+import {Application} from "../../@core/common/application";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'quickstart',
@@ -10,21 +15,20 @@ export class QuickstartPage implements OnInit {
   firstForm: FormGroup;
   secondForm: FormGroup;
   thirdForm: FormGroup;
+  key: string;
+  email: string;
+  applications: Application[] = [];
 
-  constructor(private fb: FormBuilder) {
+
+  constructor(private fb: FormBuilder, private integrationService: IntegrationService, private authService: AuthenticationService,
+              private notificationService: NotificationsService, private router: Router) {
   }
 
+
   ngOnInit() {
-    this.firstForm = this.fb.group({
-      firstCtrl: ['', Validators.required],
-    });
-
-    this.secondForm = this.fb.group({
-      secondCtrl: ['', Validators.required],
-    });
-
-    this.thirdForm = this.fb.group({
-      thirdCtrl: ['', Validators.required],
+    this.authService.getLoggedUser().subscribe(user => {
+      this.key = user.key
+      this.email = user.email
     });
   }
 
@@ -39,4 +43,18 @@ export class QuickstartPage implements OnInit {
   onThirdSubmit() {
     this.thirdForm.markAsDirty();
   }
+
+  createApplication() {
+    if (this.key) {
+      this.integrationService.createApplication({ name: "sample_app", key: this.key }).subscribe(
+        resp => {
+          this.notificationService.success('Success', 'Sample data is streaming into logsight.ai')
+          this.router.navigate(['/pages/dashboard'])
+        }, error => this.notificationService.error('Error', 'Sorry, a problem happened'))
+    } else {
+      this.notificationService.error('Error', 'Sorry, a problem happened')
+    }
+  }
+
+
 }

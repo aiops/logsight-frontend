d550996215c8eb7cbd27f55f3d85abc96963d97b
snedelkoski <sashonedelkoski@gmail.com>
2021-05-23 22:54:16 +0200

    quickstart page improvements

--- web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.module.ts
+++ web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.module.ts
@@ -6,7 +6,7 @@ import {
   NbTabsetModule,
   NbUserModule,
   NbListModule,
-  NbRouteTabsetModule, NbStepperModule, NbAccordionModule,
+  NbRouteTabsetModule, NbStepperModule, NbAccordionModule, NbProgressBarModule, NbSpinnerModule,
 } from '@nebular/theme';
 import { QuickstartRoutingModule } from './quickstart-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -15,19 +15,21 @@ import { QuickstartPage } from './quickstart.page';
 
 @NgModule({
   imports: [
-     FormsModule,
-     ReactiveFormsModule,
-     ThemeModule,
-     NbTabsetModule,
-     NbRouteTabsetModule,
-     NbStepperModule,
-     NbCardModule,
-     NbButtonModule,
-     NbListModule,
-     NbAccordionModule,
-     NbUserModule,
-     NbActionsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ThemeModule,
+    NbTabsetModule,
+    NbRouteTabsetModule,
+    NbStepperModule,
+    NbCardModule,
+    NbButtonModule,
+    NbListModule,
+    NbAccordionModule,
+    NbUserModule,
+    NbActionsModule,
     QuickstartRoutingModule,
+    NbProgressBarModule,
+    NbSpinnerModule,
   ],
   declarations: [
     QuickstartPage,
--- web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.page.ts
+++ web_app/logsight/logsight-client/src/app/pages/quickstart/quickstart.page.ts
@@ -5,6 +5,7 @@ import {AuthenticationService} from "../../auth/authentication.service";
 import {NotificationsService} from "angular2-notifications";
 import {Application} from "../../@core/common/application";
 import {Router} from "@angular/router";
+import {interval} from "rxjs";
 
 @Component({
   selector: 'quickstart',
@@ -17,6 +18,9 @@ export class QuickstartPage implements OnInit {
   thirdForm: FormGroup;
   key: string;
   email: string;
+  progressValue: number;
+  curSec: number;
+  progressIsHidden: boolean;
   applications: Application[] = [];
 
 
@@ -26,6 +30,9 @@ export class QuickstartPage implements OnInit {
 
 
   ngOnInit() {
+    this.progressIsHidden = true
+    this.progressValue = 0
+    this.curSec = 0
     this.authService.getLoggedUser().subscribe(user => {
       this.key = user.key
       this.email = user.email
@@ -47,20 +54,35 @@ export class QuickstartPage implements OnInit {
   onCardClick(){
     this.router.navigate(['/pages/integration'])
   }
+
+  startTimer(seconds: number) {
+    this.progressIsHidden = false
+    const time = seconds;
+    const timer$ = interval(1000);
+
+    const sub = timer$.subscribe((sec) => {
+      this.progressValue = Number((sec * 100 / seconds).toPrecision(1));
+      this.curSec = sec;
+      if (this.curSec === seconds) {
+        sub.unsubscribe();
+        this.router.navigate(['/pages/dashboard'])
+      }
+    });
+  }
+
   createApplication() {
     if (this.key) {
       this.integrationService.createApplication({ name: "sample_app", key: this.key }).subscribe(
         resp => {
           this.notificationService.success('Success', 'Please wait couple of minutes... sample data is streaming into logsight.ai.')
-          this.router.navigate(['/pages/dashboard'])
         }, error => this.notificationService.error('Error', 'Sorry, a problem happened'))
 
       this.integrationService.createApplication({ name: "sample_app1", key: this.key }).subscribe(
         resp => {
           // this.notificationService.success('Success', 'Please wait couple of minutes... sample data is streaming into logsight.ai.')
-          this.router.navigate(['/pages/dashboard'])
         }, error => this.notificationService.error('Error', 'Sorry, a problem happened'))
-
+      this.startTimer(120)
+      // this.router.navigate(['/pages/dashboard'])
       // this.integrationService.createApplication({ name: "sample_app2", key: this.key }).subscribe(
       //   resp => {
       //     // this.notificationService.success('Success', 'Please wait couple of minutes... sample data is streaming into logsight.ai.')
@@ -69,6 +91,8 @@ export class QuickstartPage implements OnInit {
     } else {
       this.notificationService.error('Error', 'Sorry, a problem happened')
     }
+
+
   }
 
 

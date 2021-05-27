e344be10dcd7ef65f2046ad84b427f3bfe5746f7
snedelkoski <sashonedelkoski@gmail.com>
2021-05-27 16:49:25 +0200

    internal ui improvements

--- web_app/logsight/logsight-client/src/app/pages/charts-wrapper-module/grouped-vertical-bar-chart-2d/grouped-vertical-bar-chart2d.component.ts
+++ web_app/logsight/logsight-client/src/app/pages/charts-wrapper-module/grouped-vertical-bar-chart-2d/grouped-vertical-bar-chart2d.component.ts
@@ -20,7 +20,7 @@ export class GroupedVerticalBarChart2dComponent {
   legendTitle: string = '';
 
   colorScheme = {
-    domain: ['#f0ff00', '#d94800', '#ff006e', '#8338ec', '#3a86ff']
+    domain: ['#00ff00', '#d94800', '#ff006e', '#8338ec', '#3a86ff']
   };
 
   constructor() {

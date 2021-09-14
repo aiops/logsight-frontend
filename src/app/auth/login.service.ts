370b75fa71e259f6f73a2b8c597142b8ac5a072e
Sasho Nedelkoski <sashonedelkoski@gmail.com>
2021-09-14 17:43:36 +0200

    changed activation to POST (#13)
    Co-authored-by: Alexander Acker <alexander.acker@tu-berlin.de>

--- src/app/auth/login.service.ts
+++ src/app/auth/login.service.ts
@@ -29,7 +29,7 @@ export class LoginService {
   }
 
   activate(activateForm: UserActivateForm ): any {
-    return this.apiService.put('/api/auth/activate', activateForm)
+    return this.apiService.post('/api/auth/activate', activateForm)
   }
 
   getUser(): Observable<LogsightUser> {

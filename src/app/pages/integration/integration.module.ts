bdb3bf0704412a6927f2fd415f57c915931f1f6b
snedelkoski <sashonedelkoski@gmail.com>
2021-12-07 16:08:06 +0100

    ingest logs change

--- src/app/pages/integration/integration.module.ts
+++ src/app/pages/integration/integration.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { ChartModule } from 'angular2-chartjs';
-import {NbButtonModule, NbCardModule, NbInputModule, NbSelectModule, NbTreeGridModule} from '@nebular/theme';
+import {
+    NbButtonModule,
+    NbCardModule,
+    NbInputModule,
+    NbListModule,
+    NbSelectModule,
+    NbTreeGridModule
+} from '@nebular/theme';
 import { IntegrationPage } from './integration.page';
 import { IntegrationRoutingModule } from './integration-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -10,20 +17,21 @@ import {HIGHLIGHT_OPTIONS, HighlightModule} from "ngx-highlightjs";
 import {TourMatMenuModule} from "ngx-ui-tour-md-menu";
 
 @NgModule({
-  imports: [
-    IntegrationRoutingModule,
-    ThemeModule,
-    NgxChartsModule,
-    ChartModule,
-    NbCardModule,
-    NbInputModule,
-    NbButtonModule,
-    ReactiveFormsModule,
-    HighlightModule,
-    TourMatMenuModule,
-    NbSelectModule,
-    NbTreeGridModule
-  ],
+    imports: [
+        IntegrationRoutingModule,
+        ThemeModule,
+        NgxChartsModule,
+        ChartModule,
+        NbCardModule,
+        NbInputModule,
+        NbButtonModule,
+        ReactiveFormsModule,
+        HighlightModule,
+        TourMatMenuModule,
+        NbSelectModule,
+        NbTreeGridModule,
+        NbListModule
+    ],
   declarations: [
     IntegrationPage,
   ],

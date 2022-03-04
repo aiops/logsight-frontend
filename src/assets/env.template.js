12ea5d2f0f400adec2e470c787e187fda143200e
snedelkoski <sashonedelkoski@gmail.com>
2022-03-04 14:00:30 +0100

    web-service as default variable

--- src/assets/env.template.js
+++ src/assets/env.template.js
@@ -1,6 +1,6 @@
 (function(window) {
   window.env = window.env || {};
   // Environment variables
-  window["env"]["production"] = ("${LOGSIGHT_DEPLOYMENT}" === "true");
+  window["env"]["production"] = ("${LOGSIGHT_DEPLOYMENT}" === "web-service");
   window["env"]["kibanaUrl"] = "${LOGSIGHT_FRONTEND_KIBANA_URL}";
 })(this);

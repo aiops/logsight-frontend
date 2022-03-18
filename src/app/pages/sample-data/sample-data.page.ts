5f9229fb85d33454907f8a5f4c410fb84340d376
snedelkoski <sashonedelkoski@gmail.com>
2022-03-18 20:34:15 +0100

    demo data now loads after flush

--- src/app/pages/sample-data/sample-data.page.ts
+++ src/app/pages/sample-data/sample-data.page.ts
@@ -6,6 +6,10 @@ import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {TourService} from "ngx-ui-tour-md-menu";
 import {ApiService} from "../../@core/service/api.service";
+import {Receipt} from "../../@core/common/receipt";
+import {defer, Observable} from "rxjs";
+import {delay, expand, filter, repeatWhen, retry, take, takeWhile, timeout, toArray} from "rxjs/operators";
+import {Flush} from "../../@core/common/flush";
 
 @Component({
   selector: 'sample-data',
@@ -14,6 +18,7 @@ import {ApiService} from "../../@core/service/api.service";
 })
 export class SampleDataPage implements OnInit {
   isSpinning = false;
+  listResults = []
 
   constructor(private integrationService: IntegrationService, private authService: AuthenticationService, private apiService: ApiService,
               private notificationService: NotificationsService, private http: HttpClient, private router: Router, private tourService: TourService) {
@@ -22,16 +27,35 @@ export class SampleDataPage implements OnInit {
   ngOnInit(): void {
   }
 
+
+  redirectToDashboard(result, size) {
+    this.listResults.push(result)
+    if (this.listResults.length == size) {
+      this.isSpinning = false
+      this.router.navigate(['/pages', 'dashboard'])
+      if (!localStorage.getItem("loadTour")) {
+        localStorage.setItem("loadTour", "true")
+        this.tourService.start()
+
+      }
+    }
+  }
+
   requestSampleData() {
     this.isSpinning = true
-    this.http.post(`/api/v1/demo/hadoop`, {})
-      .subscribe(resp => {
-        this.isSpinning = false
-        this.router.navigate(['/pages', 'dashboard'])
-        if (!localStorage.getItem("loadTour")){
-          localStorage.setItem("loadTour", "true")
-          this.tourService.start()
+    this.http.post<Receipt[]>(`/api/v1/demo/hadoop`, {})
+      .subscribe(receipts => {
+        for (let i = 0; i < receipts.length; i++) {
+          this.http.post<Flush>(`/api/v1/logs/flush`, {"receiptId": receipts[i].receiptId}).subscribe(flush => {
+            this.http.get<Flush>(`/api/v1/logs/flush/${flush.flushId}`).pipe(
+              repeatWhen(obs => obs),
+              filter(data => data.status == "DONE"),
+              take(1)
+            ).subscribe(result => this.redirectToDashboard(result, receipts.length));
+          })
         }
+
+
       }, error => {
         this.isSpinning = false
         this.apiService.handleErrors(error)

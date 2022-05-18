2071730be584a97d9769aac7ecb0c4cb752a1d3e
Alexander Yonchev <24881674+ayonchev@users.noreply.github.com>
2022-05-18 16:19:32 +0200

    Add wrapper component.

---
+++ src/app/verification/verification.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-verification',
+  templateUrl: './verification.component.html',
+  styleUrls: ['./verification.component.scss']
+})
+export class VerificationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

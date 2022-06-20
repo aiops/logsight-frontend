8baebc954cb323602f28cba553cd6fcf135acc4a
Sasho Nedelkoski <sashonedelkoski@gmail.com>
2022-06-20 09:36:19 +0200

    fixing issue with home (#117)

--- src/app/pages/pages-menu.ts
+++ src/app/pages/pages-menu.ts
@@ -2,15 +2,10 @@ import { NbMenuItem } from '@nebular/theme';
 
 export const MENU_ITEMS: NbMenuItem[] = [
   {
-    title: 'Dashboard',
-    icon: 'grid-outline',
-    link: '/pages/dashboard',
-    home: true
-  },
-  {
-    title: 'Incidents',
+    title: 'Compare',
     icon: 'radio-button-on-outline',
-    link: '/pages/incidents',
+    link: '/pages/compare',
+    home: true
   },
   {
     title: 'File upload',

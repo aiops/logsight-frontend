5ed558e03dd7fa2a392990a5c332ac8393198980
ayonchev <24881674+ayonchev@users.noreply.github.com>
2022-07-12 20:54:33 +0200

    Create verification cleanup.

--- src/app/verification/create-verification/create-verification.component.ts
+++ src/app/verification/create-verification/create-verification.component.ts
@@ -1,11 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {VerificationService} from '../services/verification.service';
-import {Tag} from "../../@core/common/tags";
-import {TagRequest, TagValueRequest} from "../../@core/common/TagRequest";
-import {TagEntry} from "../../@core/common/TagEntry";
 import {VerificationRequest} from "../../@core/common/verification-request";
 import {ApiService} from "../../@core/service/api.service";
 import {VerificationSharingService} from "../services/verification-sharing.service";
+import { TagType } from '../models/tag.model';
 
 @Component({
   selector: 'create-verification',
@@ -13,133 +11,20 @@ import {VerificationSharingService} from "../services/verification-sharing.servi
   styleUrls: ['./create-verification.component.scss']
 })
 export class CreateVerificationComponent implements OnInit {
+  baselineTagType = TagType.Baseline;
+  candidateTagType = TagType.Candidate;
 
-  baselineTags: Tag[];
-  baselineTagValues: Tag[];
-  baselineTagId: string;
   baselineTagMap = new Map<string, string>();
-  baselineTagMapKeys = [];
-  baselineTagKey: string;
-
-  candidateTags: Tag[];
-  candidateTagValues: Tag[];
-  candidateTagId: string;
   candidateTagMap = new Map<string, string>();
-  candidateTagMapKeys = [];
-  candidateTagKey: string;
 
   isLoading = false;
 
-  constructor(private verificationService: VerificationService, private apiService: ApiService, private verificationSharingService: VerificationSharingService) {
-  }
+  constructor(
+    private verificationService: VerificationService, 
+    private apiService: ApiService, 
+    private verificationSharingService: VerificationSharingService) { }
 
-  ngOnInit(): void {
-    this.loadAvailableTagKeys(new TagRequest([]))
-  }
-
-  loadAvailableTagKeys(tagRequest: TagRequest) {
-    this.loadAvailableBaselineTagKeys(tagRequest)
-    this.loadAvailableCandidateTagKeys(tagRequest)
-  }
-
-  baselineTagKeySelected(event) {
-    this.baselineTagKey = event.value
-    this.loadBaselineTagValuesForKey(this.baselineTagKey)
-  }
-
-
-  loadBaselineTagValuesForKey(tagKey: string) {
-    let baselineTagList = []
-    for (let tagK of this.baselineTagMap.keys()) {
-      baselineTagList.push(new TagEntry(tagK, this.baselineTagMap.get(tagK)))
-    }
-    this.verificationService.loadTagValueForKey(new TagValueRequest(tagKey, "*", baselineTagList)).subscribe(resp => {
-      this.baselineTagValues = resp.tagValues
-    })
-  }
-
-  baselineTagValueSelected(event) {
-    this.baselineTagId = event.value
-  }
-
-  setBaselineTagKeyValue() {
-    this.baselineTagValues = []
-    if(this.baselineTagKey && this.baselineTagId){
-      this.baselineTagMap.set(this.baselineTagKey, this.baselineTagId)
-    if (!this.baselineTagMapKeys.includes(this.baselineTagKey)) {
-      this.baselineTagMapKeys.push(this.baselineTagKey)
-    }
-    let baselineTagList = []
-    for (let tagK of this.baselineTagMap.keys()) {
-      baselineTagList.push(new TagEntry(tagK, this.baselineTagMap.get(tagK)))
-    }
-    this.baselineTagKey = null
-    this.baselineTagId = null
-    this.loadAvailableBaselineTagKeys(new TagRequest(baselineTagList))
-    }else{
-      this.apiService.handleNotification("Please select tag name and value!")
-    }
-
-  }
-
-  onBaselineTagRemove(event) {
-    this.baselineTagMap.delete(event.text.split(':')[0])
-    this.baselineTagMapKeys = []
-    this.baselineTagMap.forEach((value, key) => {
-      this.baselineTagMapKeys.push(key)
-    })
-    let baselineTagList = []
-    for (let tagK of this.baselineTagMap.keys()) {
-      baselineTagList.push(new TagEntry(tagK, this.baselineTagMap.get(tagK)))
-    }
-    this.loadAvailableBaselineTagKeys(new TagRequest(baselineTagList))
-  }
-
-  loadAvailableBaselineTagKeys(tagRequest: TagRequest) {
-    this.verificationService.loadAvailableTagKeys(tagRequest).subscribe(resp => {
-      this.baselineTags = resp.tagKeys
-    })
-  }
-
-  loadCandidateTagValueForKey(tagKey: string) {
-    let candidateTagList = []
-    for (let tagK of this.candidateTagMap.keys()) {
-      candidateTagList.push(new TagEntry(tagK, this.candidateTagMap.get(tagK)))
-    }
-    this.verificationService.loadTagValueForKey(new TagValueRequest(tagKey, "*", candidateTagList)).subscribe(resp => {
-      this.candidateTagValues = resp.tagValues
-    })
-  }
-
-  candidateTagValueSelected(event) {
-    this.candidateTagId = event.value
-  }
-
-  candidateTagKeySelected(event) {
-    this.candidateTagKey = event.value
-    this.loadCandidateTagValueForKey(this.candidateTagKey)
-  }
-
-
-  setCandidateTagKeyValue() {
-    this.candidateTagValues = []
-    if(this.candidateTagKey && this.candidateTagId){
-          this.candidateTagMap.set(this.candidateTagKey, this.candidateTagId)
-    if (!this.candidateTagMapKeys.includes(this.candidateTagKey)) {
-      this.candidateTagMapKeys.push(this.candidateTagKey)
-    }
-    let candidateTagList = []
-    for (let tagK of this.candidateTagMap.keys()) {
-      candidateTagList.push(new TagEntry(tagK, this.candidateTagMap.get(tagK)))
-    }
-    this.candidateTagKey = null
-    this.candidateTagId = null
-    this.loadAvailableCandidateTagKeys(new TagRequest(candidateTagList))
-    }else{
-      this.apiService.handleNotification("Please select tag name and value!")
-    }
-
-  }
+  ngOnInit(): void { }
 
   createVerification() {
     this.isLoading = true
@@ -162,26 +47,4 @@ export class CreateVerificationComponent implements OnInit {
       this.apiService.handleErrors(error)
     })
   }
-
-
-  onCandidateTagRemove(event) {
-    this.candidateTagMap.delete(event.text.split(':')[0])
-    this.candidateTagMapKeys = []
-    this.candidateTagMap.forEach((value, key) => {
-      this.candidateTagMapKeys.push(key)
-    })
-    let candidateTagList = []
-    for (let tagK of this.candidateTagMap.keys()) {
-      candidateTagList.push(new TagEntry(tagK, this.candidateTagMap.get(tagK)))
-    }
-    this.loadAvailableCandidateTagKeys(new TagRequest(candidateTagList))
-  }
-
-
-  loadAvailableCandidateTagKeys(tagRequest: TagRequest) {
-    this.verificationService.loadAvailableTagKeys(tagRequest).subscribe(resp => {
-      this.candidateTags = resp.tagKeys
-    })
-  }
-
 }
